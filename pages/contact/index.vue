ad0626df241488be64432b4ee7b20091a96f1306
jean-bertrand <juwiling@gmail.com>
2018-05-08 01:54:04 -0400

    netlify form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,6 +1,24 @@
 <template>
   <section class="container-fluid">
     <Navigation/>
+    <div class="body-container">
+      <main class="container wrapper">
+        <div class="headline">
+          <h1>Contact Me</h1>
+        </div>
+        <form name="contact" netlify>
+          <p>
+            <label>Name <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </main>
+    </div>
   </section>
 </template>
 
